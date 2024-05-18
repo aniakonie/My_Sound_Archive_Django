c70eec5a454e824900c0a65391bc6a5401d548c9
Anna Konieczka <konieczka.ania@gmail.com>
2024-05-18 09:19:57 +0200

    upgraded django, gunicorn

--- website/settings/production.py
+++ website/settings/production.py
@@ -10,7 +10,7 @@ SECURE_HSTS_SECONDS = 3153600 # 1 year
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-ALLOWED_HOSTS = ['mysoundarchive.com', 'www.mysoundarchive.com']
+ALLOWED_HOSTS = ['mysoundarchive.com', 'www.mysoundarchive.com', 'my-sound-archive-django.onrender.com']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
