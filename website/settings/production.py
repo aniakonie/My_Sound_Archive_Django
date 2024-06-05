5918fc60a6d2758cbf16633ac95ea089b2958d4d
Anna <konieczka.ania@gmail.com>
2024-06-05 18:11:20 +0100

    lp5

--- website/settings/production.py
+++ website/settings/production.py
@@ -1,6 +1,6 @@
 from website.settings.base import *
 
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
@@ -10,11 +10,7 @@ SECURE_HSTS_SECONDS = 3153600 # 1 year
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-ALLOWED_HOSTS = [
-    'mysoundarchive.com',
-    'www.mysoundarchive.com',
-    'my-sound-archive-django.onrender.com'
-]
+ALLOWED_HOSTS = ['mysoundarchive.com', 'www.mysoundarchive.com', 'my-sound-archive-django.onrender.com']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
