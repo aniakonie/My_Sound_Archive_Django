595c54b5221750f705ebc8df024b248300d7b761
Anna <konieczka.ania@gmail.com>
2024-06-05 14:51:44 +0100

    added missing load_dotenv

--- website/settings/development.py
+++ website/settings/development.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
 import dj_database_url
 
 from website.settings.base import *
 
+load_dotenv()
 
 database_url = os.getenv('DATABASE_URL')
 DATABASES['default'] = dj_database_url.parse(database_url)
